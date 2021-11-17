7afd7ad50147fcd1bc1add77246adb7dba467a7b
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2021-11-17 22:01:58 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Basically importing and initializing
+import pygame
+pygame.init()
+
+# Setting up window
+screen = pygame.display.set_mode((500,500))
+
+pygame.display.set_caption("Sea Cleaning Life")
